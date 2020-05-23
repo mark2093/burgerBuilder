d276ac5a0b23fe89ce59eb571b35e258edbd9cda
mark2093 <markdsouza93@gmail.com>
2020-05-23 20:25:32 +0530

    feat/Modal & Backdrop

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import './BuildControl.css'
 
-const buildControl = (props) =>(
+import './BuildControl.css';
 
+const buildControl = (props) => (
     <div className="BuildControl">
-        <div className="Label">{props.label}</div>
-        <button  className="Less" onClick={props.remove} disabled={props.disabled}>Less</button >
-        <button  className="More" onClick={props.added}>More</button >
+        <div className={"Label"}>{props.label}</div>
+        <button 
+            className="Less" 
+            onClick={props.removed} 
+            disabled={props.disabled}>Less</button>
+        <button 
+            className="More" 
+            onClick={props.added}>More</button>
     </div>
-
 );
 
 export default buildControl;
