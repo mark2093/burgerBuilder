b842efc74735da5e09de99f9cbb14898ab3e2ca8
mark2093 <markdsouza93@gmail.com>
2020-06-21 21:40:11 +0530

    Modified auth files

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB_19PEy5OWMpjhN5CWdDSA5VPNT5bP01U';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB_19PEy5OWMpjhN5CWdDSA5VPNT5bP01U';
         }
         axios.post(url, authData)
             .then(response => {
